53039db61a4ea0d7f69c0148500974ada8c57040
OgShadoww <galenzao@gmail.com>
2025-08-10 07:03:53 +0300

    Starter message

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 
 void startMessage() {
-  printf("Hello \n");
+  printf("╔════════════════════════════════════════╗\n");
+  printf("║                🧠 GRIND                ║\n");
+  printf("╚════════════════════════════════════════╝\n\n");
+  printf("Turn your real-life grind into a game.\nEarn XP for productive actions.\nSpend XP for things you love.\nLevel up your life.\n");
+
+  printf("Commands:\n");
+  printf("\txp add <task>\n");
+  printf("\txp use <reward>\n");
+  printf("\txp show\n\n\n");
+
+  printf("Fully offline. Simple file storage.\n");
+  printf("Lightweight C code — runs anywhere.\n\n");
+
+  printf("\"Don’t just live. Level up.\"\n");
 }
 
 int main(int arg, char **args) {
