72d530649a2e4ef2dad91c9c5d6d20b42d77aac5
OgShadoww <galenzao@gmail.com>
2025-08-10 20:38:01 +0300

    Files

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
+#include<string.h>
+
+int points;
 
 void start_message() {
   printf("╔════════════════════════════════════════╗\n");
   printf("║                🧠 GRIND                ║\n");
   printf("╚════════════════════════════════════════╝\n\n");
   printf("Turn your real-life grind into a game.\nEarn XP for productive actions.\nSpend XP for things you love.\nLevel up your life.\n");
+
+  printf("Your current point: %d", points);
 
   printf("Commands:\n");
   printf("\txp add <task>\n");
@@ -18,34 +23,38 @@ void start_message() {
 }
 
 void open_file() {
+  FILE *point = fopen("point.txt", "r");
+  fscanf(point, "%d", &points);
 
+  fclose(point);
 }
 
-void add_xp() {
-
+void add_xp(char *arg) {
+  FILE *tasks = fopen("tasks.txt", "r");
 }
 
-void use_xp() {
-  
+void use_xp(char *arg) {
+   
 }
 
-void show_xp() {
+void show_xp(char *arg) {
 
 }
 
 void emit_function(char *function, char *arg) {
-  if(function == "add") {
+  if(strcmp(function, "add")) {
     add_xp(arg);
   }
-  if(function == "use") {
-    use_xp();
+  if(strcmp(function, "use")) {
+    use_xp(arg);
   }
-  if(function == "show") {
-    show_xp(); 
+  if(strcmp(function, "show")) {
+    show_xp(arg); 
   }
 }
 
 int main(int arg, char **args) {
+  open_file();
   if(arg < 2) {
     start_message();
     return 0;
